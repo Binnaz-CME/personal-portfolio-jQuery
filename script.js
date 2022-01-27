3238d26b194ddba2cb4964e54fe257503c7526aa
Binnaz Akyüz <binnaz.akyuz@cmeducations.se>
2022-01-27 15:44:20 +0100

    replaced custom slider with slick-slider

--- script.js
+++ script.js
@@ -1,96 +1,27 @@
-'use strict';
-
-let imgIndex = 0;
-let imgPosition = 0;
-
-$(".menu").click(function () {
-  $(".menu").toggleClass("hide-menu");
-  $(".list-item").toggleClass("hide-menu");
+$('.slider').slick({
+  dots: true,
+  prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+  nextArrow: '<button class="slide-arrow next-arrow"></button>',
 });
 
-$(".btn-next").click(function (e) {
-  e.stopPropagation();
-  // console.log(e.target)
-  if (imgPosition <= 300) {
-    imgPosition += 100;
-  } else {
-    imgPosition = 0;
-  }
-
-  $("figure").css("left", -imgPosition + "%");
-
-  imgIndex++;
-
-  switch (imgIndex) {
-    case 1:
-      $("#indicator1").addClass("current");
-      $("#indicator0").removeClass("current");
-      break;
-    case 2:
-      $("#indicator2").addClass("current");
-      $("#indicator1").removeClass("current");
-      break;
-    case 3:
-      $("#indicator3").addClass("current");
-      $("#indicator2").removeClass("current");
-      break;
-    case 4:
-      $("#indicator4").addClass("current");
-      $("#indicator3").removeClass("current");
-      break;
-    default:
-      $("#indicator0").addClass("current");
-      $("#indicator4").removeClass("current");
-      imgIndex = 0;
-  }
+$('.menu').click(function () {
+  $('.menu').toggleClass('hide-menu');
+  $('.list-item').toggleClass('hide-menu');
 });
 
-$(".btn-prev").click(function (e) {
-  e.stopPropagation();
-  if (imgPosition >= 100) {
-    imgPosition -= 100;
-  } else {
-    imgPosition = 400;
-  }
+$('.sliderimg').click(function (e) {
+  $('#myModal').addClass('modal');
+  $('.sliderimg').addClass('modal-content');
 
-  if (imgIndex === 0) {
-    imgIndex = 4;
-  } else {
-    imgIndex--;
-  }
-
-  $("figure").css("left", -imgPosition + "%");
-
-  switch (imgIndex) {
-    case 1:
-      $("#indicator1").addClass("current");
-      $("#indicator2").removeClass("current");
-      break;
-    case 2:
-      $("#indicator2").addClass("current");
-      $("#indicator3").removeClass("current");
-      break;
-    case 3:
-      $("#indicator3").addClass("current");
-      $("#indicator4").removeClass("current");
-      break;
-    case 4:
-      $("#indicator4").addClass("current");
-      $("#indicator0").removeClass("current");
-      break;
-    default:
-      $("#indicator0").addClass("current");
-      $("#indicator1").removeClass("current");
-      imgIndex = 0;
-  }
+  // if (e.target === e.currentTarget) {
+  //   $('#myModal').removeClass('modal');
+  //   $('.sliderimg').removeClass('modal-content');
+  // }
 });
 
-$("#myModal").click(function (e) {
-  $("#myModal").addClass("modal");
-  $("figure img").addClass("modal-content");
-
-  if (e.target === e.currentTarget) {
-    $("#myModal").removeClass("modal");
-    $("figure img").removeClass("modal-content");
+$('body').keydown(function (e) {
+  if (e.key === 'Escape') {
+    $('#myModal').removeClass('modal');
+    $('div > img').removeClass('modal-content');
   }
 });
